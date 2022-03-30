fcff6c3be88f9fc988665d7babd16aebda14376c
Sasha <sasha@thisminute.org>
2022-03-30 17:36:12 -0400

    serve index.html and the wasm package

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_files::Files;
+use actix_web::{middleware::Logger, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	HttpServer::new(|| App::new().service(fs::Files::new("/", "pkg").show_files_listing()))
-		.bind(("127.0.0.1", 8080))?
-		.run()
-		.await
+	env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+	log::info!("starting HTTP server at http://localhost:8080");
+
+	HttpServer::new(|| {
+		App::new()
+			.service(Files::new("/cui", "./app/pkg").show_files_listing())
+			.service(Files::new("/", "./app/target/html").index_file("index.html"))
+			.wrap(Logger::default())
+	})
+	.bind(("127.0.0.1", 8080))?
+	.run()
+	.await
 }
